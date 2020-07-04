fe5bebbb31fbd7ce7d7639ec8abec46b2a4f12c1
yashgoel28 <67829870+yashgoel28@users.noreply.github.com>
2020-07-04 20:57:57 +0530

    Add files via upload

--- essarrfinance/settings.py
+++ essarrfinance/settings.py
@@ -46,12 +46,16 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'essarrfinance.urls'
@@ -129,7 +133,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
-STATIC_ROOT= os.path.join(BASE_DIR,'assets')
+STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
 
 MEDIA_URL ='/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR,'media')
